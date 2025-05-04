7b76d164c5527bba82d7a4a7b964f024b6d9bd85
Daniel <danielwick12@hotmail.com>
2025-05-03 22:19:36 -0300

    feat: enhance Fan schema with detailed document upload fields and analysis properties

--- backend/src/fan/fan.schema.ts
+++ backend/src/fan/fan.schema.ts
@@ -8,6 +8,7 @@ class DocumentDetails {
 
 @Schema()
 export class Fan extends Document {
+  // forms
   @Prop() fullName: string;
   @Prop() nickname: string;
   @Prop() email: string;
@@ -22,26 +23,43 @@ export class Fan extends Document {
   @Prop() influencers: string;
   @Prop() events: string;
   @Prop() favoriteGame: string;
-  @Prop() faceit: string;
-  @Prop() hltv: string;
+  @Prop() instagram: string;
+  @Prop() x: string;
   @Prop() others: string;
   @Prop() exclusiveContent: string;
   @Prop() message: string;
 
-  @Prop({ type: MongooseSchema.Types.Mixed }) document: DocumentDetails; // Explicit type
-  @Prop({ type: MongooseSchema.Types.Mixed }) selfie: object;
+  // Campos de upload de documentos
+  @Prop({ type: MongooseSchema.Types.Mixed }) document: { 
+    type: string; 
+    data: Buffer; 
+  }; // Explicit type for uploaded document
+  @Prop({ type: MongooseSchema.Types.Mixed }) selfie: { 
+    type: string; 
+    data: Buffer; 
+  }; // Explicit type for uploaded selfie
 
-  // Campos para tratamento posterior por IA
+  // Campos de análise de documentos
   @Prop({ default: 'pending' }) documentStatus: string; // 'pending', 'verified', 'rejected'
-  @Prop({ default: null }) documentScore: number;
+  @Prop({ default: null }) documentReport: string; // report of the document analysis
+  @Prop({ default: 'pending' }) selfieStatus: string; // 'pending', 'verified', 'rejected'
   @Prop({ default: null }) selfieMatchScore: number;
+  analysis;
+  @Prop({ default: 'pendind' }) fanStatus: string; // 'verified', 'not_verified', 'suspicious', 'pending'
 
-  // Perfil IA gerado
+  // Campos para tratamento  por IA
   @Prop({ default: null }) fanType: string; // 'casual', 'hardcore', 'colecionador', etc.
   @Prop({ default: null }) engagementScore: number;
   @Prop({ default: null }) contentPreference: string;
   @Prop({ default: null }) potentialRevenue: string;
   @Prop({ default: null }) recommendationSummary: string;
+  @Prop({ default: null }) personalChatbot: string;
+
+  // Campos de controle
+  @Prop({ default: false }) type: string; // 'fan', 'admin'
+  @Prop({ default: Date.now }) createdAt: Date;
+  @Prop({ default: Date.now }) updatedAt: Date;
+  @Prop({ default: null }) fan_id: string; // ID do fã gerado pelo sistema
 }
 
 export const FanSchema = SchemaFactory.createForClass(Fan);
